82e4070890aa41332d95ec91677d603f3166946b
Josefina Tuomala <josefina.i.tuomala@utu.fi>
2024-07-22 15:51:17 +0300

    tehtävä valmis

---
+++ osa2/Kurssitiedot/src/Komponentit/course.jsx
@@ -0,0 +1,45 @@
+const Course=({course})=>{
+    const yhteensa= course.parts.reduce((sum, part)=> sum+part.exercises, 0)
+    
+    return(
+        <div>
+            <Header name={course.name}/>
+            <Content parts={course.parts}/>
+            <Yhteensa yhteensa={yhteensa}/>
+        </div>
+    )
+}
+
+const Header=({name})=>{
+    return(
+        <h2>{name}</h2>
+    )
+}
+
+const Content=({parts})=>{
+    return(
+        <div>
+            {parts.map(part=>(
+                <Part key={part.id} part={part}/>
+            ))}
+        </div>
+    )
+}
+
+const Part=({part})=>{
+    return(
+        <p>
+            {part.name} {part.exercises}
+        </p>
+    )
+}
+
+const Yhteensa=({yhteensa})=>{
+    return(
+        <p>
+            <b>total of {yhteensa} exercises</b>
+        </p>
+    )
+}
+
+export default Course
