cda4e23b2adcd89d7a10545e42067dc73825a902
Josefina Tuomala <josefina.i.tuomala@utu.fi>
2024-07-23 10:30:42 +0300

    Valmis

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const Filter=({filter, handleFilterChange})=>{
   return(
@@ -36,15 +38,18 @@ const Persons=({personsEsilla})=>{
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const[newNumber, setNewNumber]=useState('')
   const[filter, setFilter]=useState('')
+
+  useEffect(()=>{
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then(response=>{
+        setPersons(response.data)
+      })
+      
+  },[])
 
   const addPerson=(event)=>{
     event.preventDefault()
