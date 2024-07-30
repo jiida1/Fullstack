4137828be43cbaa4d1863e405feff90feac95eff
Josefina Tuomala <josefina.i.tuomala@utu.fi>
2024-07-30 14:18:53 +0300

    4.5

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -5,50 +5,51 @@ const listHelper = require('../utils/list_helper')
    
 describe('dummy',()=>{
     test('dummy returns 1', () => {
-    const blogs = []
-    const result = listHelper.dummy(blogs)
+    const result = listHelper.dummy([])
     assert.strictEqual(result, 1)
     })
 })
 
+const listWithOneBlog=[
+    {
+        title:'joopa joo',
+        author:'jepulis',
+        url:'https://www.joo-kodit.fi/',
+        likes:5,
+        __v:0
+    }
+]
+const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+      likes: 10,
+      __v: 0
+    },
+  ]
+
+  const listWithNoBlogs = []
+
 describe('total likes',()=>{
-    const listWithOneBlog=[
-        {
-            title:'joopa joo',
-            author:'jepulis',
-            url:'https://www.joo-kodit.fi/',
-            likes:5,
-            __v:0
-        }
-    ]
-    const listWithMultipleBlogs = [
-        {
-          _id: '5a422aa71b54a676234d17f8',
-          title: 'Go To Statement Considered Harmful',
-          author: 'Edsger W. Dijkstra',
-          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-          likes: 5,
-          __v: 0
-        },
-        {
-          _id: '5a422b3a1b54a676234d17f9',
-          title: 'Canonical string reduction',
-          author: 'Edsger W. Dijkstra',
-          url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-          likes: 12,
-          __v: 0
-        },
-        {
-          _id: '5a422b891b54a676234d17fa',
-          title: 'First class tests',
-          author: 'Robert C. Martin',
-          url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
-          likes: 10,
-          __v: 0
-        },
-      ]
-    
-      const listWithNoBlogs = []
+
     
       test('when list has only one blog, equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
@@ -65,3 +66,30 @@ describe('total likes',()=>{
         assert.strictEqual(result, 0)
       })
     })
+
+    describe('favorite blog', () => {
+        
+      
+        test('when list has multiple blogs, returns the one with most likes', () => {
+          const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+          assert.deepStrictEqual(result, {
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            likes: 12
+            })
+        })
+      
+        test('when list has one blog, returns that blog', () => {
+          const result = listHelper.favoriteBlog(listWithOneBlog)
+          assert.deepStrictEqual(result, {
+            title: 'joopa joo',
+            author: 'jepulis',
+            likes: 5
+          })
+        })
+      
+        test('when list has no blogs, returns null', () => {
+          const result = listHelper.favoriteBlog(listWithNoBlogs);
+          assert.strictEqual(result, null)
+        })
+      })   
