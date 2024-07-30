4c57310a7d4e2f54e04a41b18466a46580ad1b70
Josefina Tuomala <josefina.i.tuomala@utu.fi>
2024-07-30 13:30:56 +0300

    4.4

---
+++ osa4/blogilista/tests/list_helper.test.js
@@ -0,0 +1,67 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+   
+describe('dummy',()=>{
+    test('dummy returns 1', () => {
+    const blogs = []
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+    })
+})
+
+describe('total likes',()=>{
+    const listWithOneBlog=[
+        {
+            title:'joopa joo',
+            author:'jepulis',
+            url:'https://www.joo-kodit.fi/',
+            likes:5,
+            __v:0
+        }
+    ]
+    const listWithMultipleBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: '5a422b3a1b54a676234d17f9',
+          title: 'Canonical string reduction',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+          likes: 12,
+          __v: 0
+        },
+        {
+          _id: '5a422b891b54a676234d17fa',
+          title: 'First class tests',
+          author: 'Robert C. Martin',
+          url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+          likes: 10,
+          __v: 0
+        },
+      ]
+    
+      const listWithNoBlogs = []
+    
+      test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+      })
+    
+      test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithMultipleBlogs)
+        assert.strictEqual(result, 27)
+      })
+    
+      test('of empty list is zero', () => {
+        const result = listHelper.totalLikes(listWithNoBlogs)
+        assert.strictEqual(result, 0)
+      })
+    })
